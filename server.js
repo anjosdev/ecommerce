d93917d38b3e302679b0c120e007a7e82f601b84
anjosdev <viniciusanjosdev@gmail.com>
2022-11-26 22:08:21 -0300

    5/60(0)

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const http = require('http')
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
-        setHomePage(req, res);
+        setHomePage(req, res)
     }
-    if (req.url == '/username') {
-        return submitUserName(req, res);
+
+    if (req.url == '/username' && req.method.toLowerCase() == 'post') {
+        return submitUserName(req, res)
     }
 });
 
+
+// recebe request body
+// salvar num arquivo
+// redireciona para a home
 function submitUserName(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    // > recebe request body
-    // > salva num arquivo
-    // > redireciona para a home page
-    
+    res.setHeader('Content-Type', 'text/html')
+    const body = [];
+    req.on('data', (data) => {
+        body.push(data);
+    });
+
+    req.on('end', () => {
+        console.log(body);
+        const requestBody = Buffer.concat(body).toString();
+        const userName = requestBody.split('=')[1];
+        fs.writeFileSync('username.txt', userName)
+        console.log(requestBody);
+    });
+
+
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
@@ -26,21 +42,21 @@ function setHomePage(req, res) {
     <!doctype html>
     <html>
     <head>
-    <title>Pag</title>
+    <title>Home</title>
     </head>
     <body>
     <form action="/username" method="post">
     <div>
-    <label>Enter User name</label>
+    <label>Digite seu nome de usuario</label>
         <input type="text" name="username"/>
     </div>
-
     <div>
     <input type="submit" value="send"/>
+    </div>
+    </form>
     </body>
-    
-    </html>
-    `)
+    </html>    
+    `);
 }
 
 server.listen(3000);
