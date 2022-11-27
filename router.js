ec6c96babc59c355975292c96cd315d482b27fbf
anjosdev <viniciusanjosdev@gmail.com>
2022-11-27 10:33:48 -0300

    6/60[0]

---
+++ router.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+const routes = (req, res) => {
+    if (req.url == '/') {
+        setHomePage(req, res);
+    }
+    if (req.url == '/username' && req.method.toLowerCase() == "post") {
+        return submitUserName(req, res);
+    }
+}
+
+function submitUserName(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    const body = [];
+    req.on('data', (data) => {
+        body.push(data);
+    });
+
+    req.on('end', () => {
+        const requestBody = Buffer.concat(body).toString();
+        console.log(requestBody);
+        const userName = requestBody.split('=')[1];
+        fs.writeFile('username.txt', userName, () => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+        console.log(requestBody)
+    })
+}
+
+function setHomePage(req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(`
+    <!doctype html>
+    <html>
+    <head>
+    <title>Home</title>
+    </head>
+    <body>
+    <form action="/username" method="post">
+    <div>
+    <label>Digite seu nome de usuario</label>
+        <input type="text" name="username"/>
+    </div>
+    <div>
+    <input type="submit" value="send"/>
+    </body>
+    </html>
+    `)
+
+}
+
+module.exports = {
+    routes,
+    text: 'Oi'
+};
--- server.js
+++ server.js
@@ -1,62 +1,9 @@
-const http = require('http')
-const fs = require('fs');
+const http = require('http');
+const routes = require('./router')
 
-const server = http.createServer((req, res) => {
-    if (req.url == '/') {
-        setHomePage(req, res)
-    }
+const server = http.createServer(routes.routes);
+console.log(routes.text);
 
-    if (req.url == '/username' && req.method.toLowerCase() == 'post') {
-        return submitUserName(req, res)
-    }
+server.listen(3000, () => {
+    console.log("server listening at port 3000")
 });
-
-
-// recebe request body
-// salvar num arquivo
-// redireciona para a home
-function submitUserName(req, res) {
-    res.setHeader('Content-Type', 'text/html')
-    const body = [];
-    req.on('data', (data) => {
-        body.push(data);
-    });
-
-    req.on('end', () => {
-        console.log(body);
-        const requestBody = Buffer.concat(body).toString();
-        const userName = requestBody.split('=')[1];
-        fs.writeFileSync('username.txt', userName)
-        console.log(requestBody);
-    });
-
-
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-}
-
-function setHomePage(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    return res.end(`
-    <!doctype html>
-    <html>
-    <head>
-    <title>Home</title>
-    </head>
-    <body>
-    <form action="/username" method="post">
-    <div>
-    <label>Digite seu nome de usuario</label>
-        <input type="text" name="username"/>
-    </div>
-    <div>
-    <input type="submit" value="send"/>
-    </div>
-    </form>
-    </body>
-    </html>    
-    `);
-}
-
-server.listen(3000);
