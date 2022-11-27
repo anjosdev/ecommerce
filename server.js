605dc2921e7006ad10060f3ab12c9f5c781cd9aa
anjosdev <viniciusanjosdev@gmail.com>
2022-11-27 16:33:28 -0300

    10/60

--- server.js
+++ server.js
@@ -2,5 +2,21 @@ const express = require('express');
 
 const app = express();
 
+
+// app.use((req, res, next) => {
+//     console.log("first middleware");
+//     next();
+// })
+
+// app.use((req, res, next) => {
+//     console.log("second middleware");
+    
+// })
+
+app.use('/', (req, res, next) => {
+    console.log('first request');
+})
+
 app.listen(3000, () => {
-    console.log("server started at port 3000")
+    console.log("servidor rodando na porta 3000");
+});
