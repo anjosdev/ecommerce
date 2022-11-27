29f090f2a4047890feac12cc71ddacb3a741cbcd
anjosdev <viniciusanjosdev@gmail.com>
2022-11-27 18:41:19 -0300

    10/60

--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     console.log("first middleware");
-//     next();
-// })
+app.use((req, res, next) => {
+    console.log("Logging the request");
+    next();
+})
 
 // app.use((req, res, next) => {
 //     console.log("second middleware");
     
 // })
 
+app.use('/users', (req, res, next) => {
+    res.send('This is the users page')
+})
+
 app.use('/', (req, res, next) => {
-    console.log('first request');
+    res.send('<h1 style="color: red;">This is the homepage</h1>');
+    next();
 })
 
 app.listen(3000, () => {
-    console.log("server started at port 3000")
+    console.log("servidor rodando na porta 3000")
 })
-
