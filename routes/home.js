6a2b69268d887adc9f8d5b3cea03058262ca1c87
anjosdev <viniciusanjosdev@gmail.com>
2022-11-28 20:25:19 -0300

    14/60

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir = require('../utils/path');
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'homepage.html'));
 });
 
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const rootDir = require('./utils/path');
 
 const adminRoutes = require('./routes/admin');
 const homeRoutes = require('./routes/home');
 
 const app = express();
 
-// instalar npm BodyParser
+app.use(express.static(path.join(rootDir, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(homeRoutes);
 app.use('/users/', adminRoutes);
 
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 app.listen(3000, () => {
