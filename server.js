6d1243057b520faa58b3179485edf5b1481cf01a
anjosdev <viniciusanjosdev@gmail.com>
2022-11-28 16:50:48 -0300

    11/60[0]

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// instalar npm BodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-    console.log("Logging the request");
+    console.log("Logando a requisição");
     next();
 })
 
-// app.use((req, res, next) => {
-//     console.log("second middleware");
-    
-// })
+app.get('/users', (req, res, next) => {
+    res.send("<h1>Página do usuário</h1>");
 
-app.use('/users', (req, res, next) => {
-    res.send('This is the users page')
 })
 
-app.use('/', (req, res, next) => {
-    res.send('<h1 style="color: red;">This is the homepage</h1>');
-    next();
+app.get('/user/add', (req, res) => {
+    res.send(`<form method="POST">
+    <div><input name='username'/></div>
+    <div><button>Add user</button></div>
+    </form>`)
+});
+
+app.post('/user/add', (req, res) => {
+    console.log(req.body);    
+    console.log('post request ')
+    res.redirect('/');
+})
+
+app.get('/', (req, res, next) => {
+    res.send("<h1>Página principal</h1><br><h2>Bem-vindo</h2>");
 })
 
 app.listen(3000, () => {
