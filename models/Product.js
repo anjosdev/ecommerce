3b57240aa06cd782ba7c6c9d1df4259c029e11f8
anjosdev <viniciusanjosdev@gmail.com>
2022-12-02 15:02:23 -0300

    24/60[0]

--- models/Product.js
+++ models/Product.js
@@ -3,18 +3,15 @@ const path = require('path');
 const rootDir = require('../utils/path');
 
 
-
 const getProductsFromFile = (callBack) => {
     const productsPath = path.join(rootDir, 'data', 'products.json')
     fs.readFile(productsPath, (error, productsData) => {
         if (error) {
             return callBack();
         }
-
         return callBack(JSON.parse(productsData));
     });
 };
-
 
 exports.saveProduct = (product) => {
     const productsPath = path.join(rootDir, 'data', 'products.json');
