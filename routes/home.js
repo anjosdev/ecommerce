f042ddac8783d65793991decbd886412a09cb8c1
anjosdev <viniciusanjosdev@gmail.com>
2022-12-02 20:50:15 -0300

    26/60

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { postCartPage, getCartPage } = require('../controllers/CartController');
 const router = express.Router();
 const { getHomePage, getProductDetailsPage } = require('../controllers/HomeController');
 
 router.get('/', getHomePage);
 
-router.get('/products/details/:productId', getProductDetailsPage)
+router.get('/products/details/:productId', getProductDetailsPage);
+
+router.post('/cart', postCartPage);
+router.get('/cart', getCartPage);
 
 module.exports = router;
